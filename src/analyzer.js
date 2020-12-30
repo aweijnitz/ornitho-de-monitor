72209d74e6a191eb63f7bff2cb4d49204b2174e4
anders <andersw@noone.it>
2020-12-30 21:44:23 +0100

    Creates working scaffold

---
+++ src/analyzer.js
@@ -0,0 +1,22 @@
+const analyzeMsg = msgPayload => {
+  console.log('Analyzing', JSON.stringify(msgPayload));
+  console.log('Nr hits', msgPayload.hits.length);
+}
+
+/**
+ * Background Cloud Function to be triggered by Pub/Sub.
+ * This function is executed when the trigger topic receives a message.
+ *
+ * @See https://cloud.google.com/functions/docs/writing/background#cloud_pubsub_example
+ *
+ * @param {object} message The Pub/Sub message.
+ * @param {object} context The event metadata.
+ */
+exports.analyzeObservations = (message, context) => {
+  console.log('ANALYZER TRIGGERED!', JSON.stringify(message), JSON.stringify(context));
+  const payload = message.data
+    ? Buffer.from(message.data, 'base64').toString()
+    : false;
+  if (payload != false)
+    analyzeMsg(JSON.parse(payload));
+};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { analyzeObservations } = require('./analyzer');
+
+// Multiple functions: https://stackoverflow.com/questions/52732672/how-to-deploy-multiple-functions-using-gcloud-command-line
+
+module.exports = {
+  analyzeObservations
+};
