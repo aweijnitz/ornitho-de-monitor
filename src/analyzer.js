0dff74bbd74c2f708b5bdbde5bacaddfad832ccd
anders <andersw@noone.it>
2021-01-03 15:35:37 +0100

    Bugfix.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,13 +1,13 @@
 const {
-  publishNotificationMessage,
-  isObservationMessage
+   publishNotificationMessage,
+   isObservationMessage
 } = require('./utils/publishMessage');
 
 const {isSameDay, createEmptyReport} = require('./utils/reportUtils');
 
 const {
-  getLatestNReports,
-  saveReport,
+   getLatestNReports,
+   saveReport,
 } = require('./persistence/index');
 
 
@@ -18,21 +18,29 @@ const {
  * @return {Promise<void>}
  */
 const analyzeMsg = async newReport => {
-  console.log('Analyzing', JSON.stringify(newReport));
-  //console.log('hits', newReport.hits);
-  const storageRecord = await getLatestNReports(1);
-  const lastReport = storageRecord && storageRecord.length > 0 ? storageRecord[0].report : createEmptyReport();
-  console.log('lastReport', JSON.stringify(lastReport));
-  
-  if ((lastReport.md5 !== newReport.md5)
-    && isSameDay(newReport, lastReport)) {
-    // Ok, so we now know we have a report which is different from last time.
-    // If the day rolled over, the report will be different, but we don't want to be notified about it,
-    // but we got here, so the report changed and it is worth while notifying about it.
-    onsole.log('Pushing notification and saving: ', lastReport.md5, JSON.stringify(lastReport));
-    await publishNotificationMessage(newReport);
-    return saveReport(newReport);
-  }
+   console.log('Analyzing', newReport.md5, JSON.stringify(newReport));
+   const storageRecord = await getLatestNReports(1);
+   const lastReport = storageRecord && storageRecord.length > 0 ? storageRecord[0].report : createEmptyReport();
+   console.log('lastReport', lastReport.md5, JSON.stringify(lastReport));
+   if (lastReport.md5 === newReport.md5) {
+      console.log('Skipping. Reports are equal.')
+      return;
+   }
+
+   if (!isSameDay(newReport, lastReport)) {
+      console.log('Skipping notification, but saving. Reports are from different days.')
+      return saveReport(newReport);
+   }
+
+   if ((lastReport.md5 !== newReport.md5)
+      && isSameDay(newReport, lastReport)) {
+      // Ok, so we now know we have a report which is different from last time.
+      // If the day rolled over, the report will be different, but we don't want to be notified about it,
+      // but we got here, so the report changed and it is worth while notifying about it.
+      console.log('Pushing notification and saving: ', lastReport.md5, JSON.stringify(lastReport));
+      await publishNotificationMessage(newReport);
+      return saveReport(newReport);
+   }
 }
 
 /**
@@ -45,15 +53,15 @@ const analyzeMsg = async newReport => {
  * @param {object} context The event metadata.
  */
 exports.analyzeObservations = async (message, context) => {
-  console.log('ANALYZER TRIGGERED!', JSON.stringify(message), JSON.stringify(context));
-  if (!isObservationMessage(message)) {
-    console.log('Analyzer skipping message.', message);
-    return;
-  }
-  const payload = message.data
-    ? Buffer.from(message.data, 'base64').toString()
-    : false;
-  
-  if (payload !== false)
-    return await analyzeMsg(JSON.parse(payload));
+   console.log('ANALYZER TRIGGERED!', JSON.stringify(message), JSON.stringify(context));
+   if (!isObservationMessage(message)) {
+      console.log('Analyzer skipping message.', message);
+      return;
+   }
+   const payload = message.data
+      ? Buffer.from(message.data, 'base64').toString()
+      : false;
+
+   if (payload !== false)
+      return await analyzeMsg(JSON.parse(payload));
 };
